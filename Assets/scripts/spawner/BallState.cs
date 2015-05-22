e5d894c15b54cf5c0898c499f441d1e4d2a4c5e6
Julian <julian@contingent.dk>
2015-05-23 00:28:24 +0200

    meta files

---
+++ Assets/scripts/spawner/BallState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallState : MonoBehaviour {
+	
+	public bool hasPlayedFartSount = false;
+	public bool hasDealtDamage = false;
+	public float hitDist = -1f;
+	
+	public bool inHitZone = false;
+	
+	public Vector3 velocity;
+}
