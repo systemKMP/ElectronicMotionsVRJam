5be965cf758b160eeb284baee107cc7c0ab7d0ed
systemKMP <km@gearworks.dk>
2015-05-23 01:42:56 +0200

    Added collisions

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ColliderType { LeftHand, RightHand, Head }
+
+public class CollisionDetector : MonoBehaviour
+{
+
+    public ColliderType ColType;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (ColType != ColliderType.Head)
+        {
+            EffectFactory.Instance.SpawnEffect(EffectType.Simple, other.gameObject.transform.position, 2.0f);
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/EffectFactory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class EffectFactory : MonoBehaviour
+{
+    public List<Effect> Effects;
+
+    private static EffectFactory _instance;
+
+    public static EffectFactory Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    public void SpawnEffect(EffectType type, Vector3 location, float timer)
+    {
+        var eff = Effects.Find(n=>n.Type == type);
+        var obj = Instantiate(eff.Object, location, Quaternion.identity) as GameObject;
+        Destroy(obj, timer);
+    }
+
+
+
+}
+
+[Serializable]
+public class Effect
+{
+    public GameObject Object;
+    public EffectType Type;
+}
+
+public enum EffectType { Simple = 0, Super = 1 }
