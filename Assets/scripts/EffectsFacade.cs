2331b46ab9a2f8b0f4c71a82653627f06880da55
Julian <julian@contingent.dk>
2015-05-23 13:29:21 +0200

    tweak on visuals, is pretty nice

---
+++ Assets/scripts/EffectsFacade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectsFacade : MonoBehaviour {
+
+	public bool randomRotation = true;
+	public float rotationSpeed = 100;
+
+	public void Particles() {
+		
+	}
+
+	Rigidbody _rigidBody;
+
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody>();
+
+		if (randomRotation) {
+			Vector3 v = new Vector3(rotationSpeed*Random.value, rotationSpeed*Random.value, rotationSpeed*Random.value);
+			_rigidBody.angularVelocity = v;
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
