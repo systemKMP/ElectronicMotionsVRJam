45de2ed5a07496585fec6433cbcc2c7ec17bc798
Mikael <lowhifi@gmail.com>
2015-05-24 09:23:19 +0200

    New edited version of music tracks in "music3" folder

---
+++ Assets/Scripts/LPFExtension.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LPFilterExtension : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioLowPassFilter alpf = GetComponent<>(AudioLowPassFilter);
+		public float cutoff = alpf.cutoffFrequency;
+		cutoff = 22000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cutoff = 
+	}
+}
